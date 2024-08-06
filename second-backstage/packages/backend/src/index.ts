95e0d253b3f1f0a3d8eb642eb40d427656f310a2
rowan04 <rowan.moss@stfc.ac.uk>
2024-08-06 17:06:18 +0100

    progress in github integration - authentication and organisational data

--- second-backstage/packages/backend/src/index.ts
+++ second-backstage/packages/backend/src/index.ts
@@ -27,6 +27,7 @@ backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
+backend.add(import('@backstage/plugin-events-backend/alpha'));
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 
@@ -46,5 +47,11 @@ backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 // search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 backend.start();
